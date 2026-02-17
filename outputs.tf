77e224a3dacbf9b72318606e2e474a3a2c71d6e0
David Ogbonnah <davido6593@gmail.com>
2026-02-17 12:45:29 +0000

    Add outputs for ALB and internal ALB URLs

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,14 @@ output "internal_alb_dns_name" {
   value = aws_lb.arangodb_internal_alb.dns_name
 }
 
+output "alb_url" {
+  value = "http://${aws_lb.arangodb_alb.dns_name}"
+}
+
+output "internal_alb_url" {
+  value = "http://${aws_lb.arangodb_internal_alb.dns_name}"
+}
+
 output "jwt_secret_arn" {
   value = aws_secretsmanager_secret.arangodb_jwt_secret.arn
 }
